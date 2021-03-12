67b2254ef2dd81ab66251ce409e1eea21a63e1c2
MalikJia <77734809+MalikJia@users.noreply.github.com>
2021-03-12 23:20:01 +0800

    Create ans.java

---
+++ Tree/Binary Tree/235. Lowest Common Ancestor of a Binary Search Tree/ans.java
@@ -0,0 +1,80 @@
+approach 1: recursive
+![https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/Figures/235/235_LCA_Binary_1.png]
+
+根据LCA的定义. 比如, 对于3和9来说LCA是6
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        // Value of current node or parent node.
+        int parentVal = root.val;
+
+        // Value of p
+        int pVal = p.val;
+
+        // Value of q;
+        int qVal = q.val;
+
+        //根据BST的定义, 小于root的在左边, 大于root的在右边
+        if (pVal > parentVal && qVal > parentVal) {
+            // If both p and q are greater than parent
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (pVal < parentVal && qVal < parentVal) {
+            // If both p and q are lesser than parent
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            // We have found the split point, i.e. the LCA node.
+            
+            //如果p和q分别在root的两边，则此root即为LCA
+            return root;
+        }
+    }
+    
+  
+Complexity Analysis
+
+Time Complexity: O(N), where N is the number of nodes in the BST. In the worst case we might be visiting all the nodes of the BST.
+
+Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be N since the height of a skewed BST could be N.
+  
+  
+approach 2: Iterative
+  
+      public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // Value of p
+        int pVal = p.val;
+
+        // Value of q;
+        int qVal = q.val;
+
+        // Start from the root node of the tree, 同时避免原始的root被修改, 保持原始root
+        TreeNode node = root;
+
+        // Traverse the tree
+        while (node != null) {
+
+            // Value of ancestor/parent node.
+            int parentVal = node.val;
+
+            if (pVal > parentVal && qVal > parentVal) {
+                // If both p and q are greater than parent
+                node = node.right;
+            } else if (pVal < parentVal && qVal < parentVal) {
+                // If both p and q are lesser than parent
+                node = node.left;
+            } else {
+                // We have found the split point, i.e. the LCA node.
+                return node;
+            }
+        }
+        return null;
+    }
+    
+ Complexity Analysis
+
+Time Complexity : O(N), where N is the number of nodes in the BST. In the worst case we might be visiting all the nodes of the BST.
+
+Space Complexity : O(1).
+  
+  
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
