bdff77bf90f73ebaaf6185bd5c8262b87ef8c6d7
MalikJia <77734809+MalikJia@users.noreply.github.com>
2021-03-12 17:24:01 +0800

    Create ans.java

---
+++ Tree/in-order/897	 Increasing Order Search Tree/ans.java
@@ -0,0 +1,40 @@
+Approach 1: In-Order Traversal
+class Solution {    
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> vals = new ArrayList();
+        inorder(root, vals);
+        TreeNode ans = new TreeNode(0), cur = ans;
+        for (int v: vals) {
+            cur.right = new TreeNode(v);
+            cur = cur.right;
+        }
+        return ans.right;
+    }
+
+    public void inorder(TreeNode node, List<Integer> vals) {
+        if (node == null) return;
+        inorder(node.left, vals);
+        vals.add(node.val);
+        inorder(node.right, vals);
+    }
+}
+
+Approach 2: Traversal with Relinking
+class Solution {
+    TreeNode cur;
+    public TreeNode increasingBST(TreeNode root) {
+        TreeNode ans = new TreeNode(0);
+        cur = ans;
+        inorder(root);
+        return ans.right;
+    }
+
+    public void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        node.left = null;
+        cur.right = node;
+        cur = node;
+        inorder(node.right);
+    }
+}
